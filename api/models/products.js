5bbae6c37c9385b0da269aac72d548ac930b2cfc
rimseo12 <seorimhyun@gmail.com>
2021-04-26 13:35:44 +0900

    prettier 재설치

--- api/models/products.js
+++ api/models/products.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productsSchema = new Schema({
-   name: { type: String, required: true },
-   image_url: { type: String },
-   expiration_date: { type: Date, default: Date.now },
-   memo: { type: String },
-   status: { type: String, default: "active" },
-   created_date: { type: Date, default: Date.now },
+  name: { type: String, required: true },
+  image_url: { type: String },
+  expiration_date: { type: Date, default: Date.now },
+  memo: { type: String },
+  status: { type: String, default: "active" },
+  created_date: { type: Date, default: Date.now },
 },{
-    versionKey: false
+  versionKey: false
 });
 
 module.exports = mongoose.model("products", productsSchema);
